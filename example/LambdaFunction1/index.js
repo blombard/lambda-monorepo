17ce888a3c72634b9a4e7fac99039c630ede8a36
Baptiste Lombard <btlcasio@gmail.com>
2020-08-30 15:24:52 +0200

    Add an action metadata file + add examples

---
+++ example/LambdaFunction1/index.js
@@ -0,0 +1,6 @@
+exports.handler = async () => {
+  console.log('Hello World 1')
+  return {
+    statusCode: 200
+  };
+};
---
+++ example/LambdaFunction2/index.js
@@ -0,0 +1,6 @@
+exports.handler = async () => {
+  console.log('Hello World 2')
+  return {
+    statusCode: 200
+  };
+};
